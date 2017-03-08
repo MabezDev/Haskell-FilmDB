7ca71bc382a88536673e7ebc9dac4046774b9079
Mabez M <Mabez M>
2017-03-08 20:16:37 +0000

    Reworked Q6 to not use an if statement, added validation to the menu

--- coursework.hs
+++ coursework.hs
@@ -13,6 +13,7 @@
 import Control.Monad
 import Data.List
 import Data.Map (fromListWith, toList)
+import Data.Char
 
 --
 -- Types
@@ -57,12 +58,18 @@ searchFilmByFan filmdb fan =
 searchFansByFilm :: [Film] -> String -> [String] --V
 searchFansByFilm filmdb title = concat [ (getFans film) | film <- filmdb, title == (getTitle film)]
 
-addFanToFilm :: [Film] -> String -> String -> [Film] --VI
-addFanToFilm [] _ _ = []
-addFanToFilm ((Film title director year fans):xs) titleToModify fanToAdd = 
-    if title == titleToModify
-        then (Film title director year (fanToAdd : fans)) : addFanToFilm xs titleToModify fanToAdd
-        else (Film title director year fans) : addFanToFilm xs titleToModify fanToAdd
+--addFanToFilm :: [Film] -> String -> String -> [Film] --VI
+--addFanToFilm [] _ _ = []
+--addFanToFilm ((Film title director year fans):xs) titleToModify fanToAdd = 
+--    if title == titleToModify
+--        then (Film title director year (fanToAdd : fans)) : addFanToFilm xs titleToModify fanToAdd
+--        else (Film title director year fans) : addFanToFilm xs titleToModify fanToAdd
+
+addFanToFilm :: [Film] -> String -> String -> [Film]
+addFanToFilm (x:xs) title fan
+    | (length (x:xs)) == 0  = []
+    | (getTitle x) == title = (Film title (getDirector x) (getYear x) (fan:(getFans x))) : (addFanToFilm xs title fan)
+    | otherwise             = x : (addFanToFilm xs title fan)
         
 
 searchFansByDirector :: [Film] -> String -> [String] --VII
@@ -125,6 +132,7 @@ printDatabase (x:xs) = do
     
 printMenu :: IO ()
 printMenu = do
+    putStrLn ""
     putStrLn ("1) Add a new film.")
     putStrLn ("2) List all films.")
     putStrLn ("3) List all films after a certain year.")
@@ -132,8 +140,9 @@ printMenu = do
     putStrLn ("5) List all fans of a particular film.")
     putStrLn ("6) Become a fan of a film.")
     putStrLn ("7) List all fans of a director.")
-    putStrLn ("8) ??????")
+    putStrLn ("8) List directors and the number of films you are a fan of.")
     putStrLn ("9) Exit.")
+    putStr "--> "
        
 askName :: IO String
 askName = do
@@ -143,13 +152,21 @@ askName = do
 getInt :: IO Int
 getInt = do
     input <- getLine
-    return (read input :: Int)
+    if isInt input
+        then return (read input :: Int)
+        else return (-1)
+
+isInt s = case reads s :: [(Integer, String)] of
+  [(_, "")] -> True
+  _         -> False       
         
 handleInput :: [Film] -> String -> IO ()
 handleInput filmdb username = do
     printMenu
-    choice <- getLine
-    when ((read choice :: Int) == 1) $ do
+    choice <- getInt
+    putStrLn ""
+    
+    when (choice == 1) $ do
         putStrLn "Title: "
         title <- getLine
         putStrLn "Director: "
@@ -157,37 +174,44 @@ handleInput filmdb username = do
         putStrLn "Year: "
         year <- getInt
         handleInput (addFilmToDatabase filmdb title director year []) username
-    when ((read choice :: Int) == 2) $ do
+    when (choice == 2) $ do
         printDatabase filmdb
         handleInput filmdb username
-    when ((read choice :: Int) == 3) $ do
+    when (choice == 3) $ do
         putStrLn "Films after the year: "
         year <- getInt
         printDatabase (searchFilmByYearReleased filmdb year)
         handleInput filmdb username
-    when ((read choice :: Int) == 4) $ do
+    when (choice == 4) $ do
         putStrLn ("Listing all films that " ++ username ++ " is a fan of.")
         printDatabase (searchFilmByFan filmdb username)
         handleInput filmdb username
-    when ((read choice :: Int) == 5) $ do
+    when (choice == 5) $ do
         putStrLn "Film: "
         title <- getLine
         putStrLn (show (searchFansByFilm filmdb title))
         handleInput filmdb username
-    when ((read choice :: Int) == 6) $ do
+    when (choice == 6) $ do
         putStrLn "Film: "
         title <- getLine
         handleInput (addFanToFilm filmdb title username) username
-    when ((read choice :: Int) == 7) $ do
+    when (choice == 7) $ do
         putStrLn "Director: "
         director <- getLine
         putStrLn (show (searchFansByDirector filmdb director))
         handleInput filmdb username
-    when ((read choice :: Int) == 8) $ do
+    when (choice == 8) $ do
         putStrLn ("Listing all directors and the number of films " ++ username ++ " is a fan of. In the format ('Director',number).")
         putStrLn (show (searchDirectorsByFan filmdb username))
         handleInput filmdb username
-    return ()
+    when (choice == 9) $ do
+        return ()
+    when (choice == -1) $ do
+        putStr "Enter a valid Integer!"
+        putStrLn ""
+        handleInput filmdb username
+    putStrLn "Not a valid option enter an integer between 1 and 9."
+    handleInput filmdb username
     
     
     
